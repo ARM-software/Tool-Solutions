6f737fe147d18b512feb7476d4a83501b2754875
Jason Andrews <jason.r.andrews@comcast.net>
2019-02-15 20:20:22 +0000

    Rock960 scripts for Linux

--- ml-tool-examples/rock960-linux/rock960-opencl.sh
+++ ml-tool-examples/rock960-linux/rock960-opencl.sh
@@ -16,7 +16,7 @@ function IsPackageInstalled() {
 sudo apt-get update && sudo apt-get upgrade -y
 
 # packages to install
-packages="git locales dpkg-dev opencl-headers clinfo"
+packages="git locales dpkg-dev opencl-headers clinfo unzip bzip2 nano module-init-tools aptitude software-properties-common python-software-properties htop libblas-dev libgflags-dev libgoogle-glog-dev python-numpy python-dev python3 libatlas-base-dev libatlas-dev libhdf5-serial-dev libleveldb-dev liblmdb-dev libopencv-dev libsnappy-dev python3-pip python3-numpy python3-pil python3-matplotlib locales"
 for package in $packages; do
     if ! IsPackageInstalled $package; then
         sudo apt-get install -y $package
