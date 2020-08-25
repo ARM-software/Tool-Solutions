c63df191897a035360034ae6f427cfcf275002bc
andtob01 <tobias.andersson@arm.com>
2020-08-25 15:46:10 +0900

    fix warnings since they are treated as errors

--- docker/tensorflow-lite-micro-models/download_tflow_cmsis.sh
+++ docker/tensorflow-lite-micro-models/download_tflow_cmsis.sh
@@ -2,6 +2,7 @@
 
 #CMSIS-repo location
 CMSIS_DIR=./CMSIS_REPO
+TFLM_ROOT=$PWD/tensorflow/tensorflow/lite/micro
 
 #Clone the tensorflow git repo if you haven't already
 if [ ! -d "tensorflow" ]
@@ -25,9 +26,26 @@ fi
 
 
 #Copy the files to the right locations
-cp -r $PWD/TFLite_micro_IPSS_Support/helper_functions.inc $PWD/tensorflow/tensorflow/lite/micro/tools/make/
-cp -r $PWD/TFLite_micro_IPSS_Support/armclang $PWD/tensorflow/tensorflow/lite/micro/tools/make/templates
-cp -r $PWD/TFLite_micro_IPSS_Support/ipss* $PWD/tensorflow/tensorflow/lite/micro/tools/make/targets/
-cp -r $PWD/TFLite_micro_IPSS_Support/cmsis.inc $PWD/tensorflow/tensorflow/lite/micro/tools/make/ext_libs/
+cp -r $PWD/TFLite_micro_IPSS_Support/helper_functions.inc ${TFLM_ROOT}/tools/make/
+cp -r $PWD/TFLite_micro_IPSS_Support/armclang ${TFLM_ROOT}/tools/make/templates
+cp -r $PWD/TFLite_micro_IPSS_Support/ipss* ${TFLM_ROOT}/tools/make/targets/
+cp -r $PWD/TFLite_micro_IPSS_Support/cmsis.inc ${TFLM_ROOT}/tools/make/ext_libs/
 
+
+# Fix double-promotion warning in mul.cc
+sed -i -E 's#input1->params.scale \* input2->params.scale \/ output->params.scale;#static_cast<double>\(input1->params.scale\) \*\
+        static_cast<double>\(input2->params.scale\) \/\
+        static_cast<double>\(output->params.scale\);#' \
+        ${TFLM_ROOT}/kernels/cmsis-nn/mul.cc
+
+# Fix missing-field-initializers warning in mul.cc
+sed -i -E 's#return \{mul::Init, nullptr \/\* Free \*\/, mul::Prepare, mul::Eval\};#return \{mul::Init,\
+        nullptr \/\* Free \*\/,\
+        mul::Prepare,\
+        mul::Eval, \
+        \/\*profiling_string=\*\/nullptr,\
+        \/\*builtin_code=\*\/0,\
+        \/\*custom_name=\*\/nullptr,\
+        \/\*version=\*\/0};#' \
+        ${TFLM_ROOT}/kernels/cmsis-nn/mul.cc
 
