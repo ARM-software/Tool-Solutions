400a8084842fbdb61f318b4f4f8e253af8668b4b
Ubuntu <pareena.verma@arm.com>
2021-01-26 15:14:04 +0000

    Updated the tensorflow-lite-micro-fvp example to use tag 2.4.1 of tensorflow and avoid breaking changes in master

--- docker/tensorflow-lite-micro-fvp/download_tflow_cmsis.sh
+++ docker/tensorflow-lite-micro-fvp/download_tflow_cmsis.sh
@@ -2,11 +2,14 @@
 
 #CMSIS-repo location
 CMSIS_DIR=./CMSIS_REPO
+#TensorFlow Lite Micro location
+TFLM_ROOT=$PWD/tensorflow/tensorflow/lite/micro
+
 
 #Clone the tensorflow git repo if you haven't already
 if [ ! -d "tensorflow" ]
 then
-    git clone https://github.com/tensorflow/tensorflow.git
+    git clone -b v2.4.1 https://github.com/tensorflow/tensorflow.git
 else
     echo "Tensorflow repo already cloned"
 fi
@@ -29,3 +32,9 @@ cp -r $PWD/TFLite_micro_FVP_Support/helper_functions.inc $PWD/tensorflow/tensorf
 cp -r $PWD/TFLite_micro_FVP_Support/armclang $PWD/tensorflow/tensorflow/lite/micro/tools/make/templates
 cp -r $PWD/TFLite_micro_FVP_Support/fvp* $PWD/tensorflow/tensorflow/lite/micro/tools/make/targets/
 cp -r $PWD/TFLite_micro_FVP_Support/cmsis.inc $PWD/tensorflow/tensorflow/lite/micro/tools/make/ext_libs/
+
+#Remove -Wl, --fatal-warnings for Arm Compiler
+sed -i -E 's#-Werror # #' ${TFLM_ROOT}/tools/make/Makefile
+sed -i -E 's#-Wl,--fatal-warnings # #' ${TFLM_ROOT}/tools/make/Makefile
+sed -i -E 's/-Wl,--gc-sections//g' ${TFLM_ROOT}/tools/make/Makefile
+
