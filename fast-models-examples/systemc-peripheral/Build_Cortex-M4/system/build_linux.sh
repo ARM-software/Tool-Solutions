fccd0c2bb78d89860e06958b3f625f7afe2516d5
Jason Andrews <jason.r.andrews@comcast.net>
2022-01-13 18:21:51 -0600

    Enable SystemC peripheral Fast Model example on aarch64

--- fast-models-examples/systemc-peripheral/Build_Cortex-M4/system/build_linux.sh
+++ fast-models-examples/systemc-peripheral/Build_Cortex-M4/system/build_linux.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 #
 # build.sh - Build the SystemC Peripheral example
 #
@@ -6,9 +6,17 @@
 # All rights reserved.
 #
 
+arch=$(uname -m)
+
+if [ "$arch" == 'aarch64' ]; then
+    make rel_gcc63_64
+else
+    make rel_gcc73_64
+fi
+
 # select the version of gcc you use
 #make rel_gcc49_64
 #make rel_gcc64_64
-make rel_gcc73_64
+#make rel_gcc73_64
 #make rel_gcc93_64
 
