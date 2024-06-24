1ba90acac6f2d38e069b5a0f0c40138e9cea237c
Ryo Suzuki <ryo.suzuki@arm.com>
2024-06-24 11:18:36 +0000

    Enable oneDNN for tanh based GELU on aarch64
    Provides significant speedup for GELU on aarch64 e.g.:
    
    8.5x speedup compared to native implementation for 1x1x16384 on 32 threads

--- docker/pytorch-aarch64/scripts/build-pytorch.sh
+++ docker/pytorch-aarch64/scripts/build-pytorch.sh
@@ -48,8 +48,8 @@ if [[ $ONEDNN_BUILD ]]; then
 fi
 
 patch -p1 < $PACKAGE_DIR/pytorch_dynamic_quantization.patch
-patch -p1 < $PACKAGE_DIR/pytorch_gelu.patch
 patch --ignore-whitespace -p1 < $PACKAGE_DIR/pytorch_static_quantization.patch
+patch -p1 < $PACKAGE_DIR/pytorch_gelu.patch
 
 # Apply https://github.com/pytorch/pytorch/pull/122616 to make torch 2.3.0 backwards
 # compatible with torchdata
