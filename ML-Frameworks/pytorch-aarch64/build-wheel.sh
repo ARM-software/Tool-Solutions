92b85ed2d6db16cee012de984402fdc0ab73b36d
Jonathan Deakin <jonathan.deakin@arm.com>
2025-01-03 11:34:21 +0000

    Remove TEST_ENV from build-wheel.sh
    Remove TEST_ENV and install build deps directly in docker container,
    this is what upstream CI does. Our test environment is the
    _Tool-Solutions_ docker container (defined in
    ML-Frameworks/pytorch-aarch64/Dockerfile), the build environment should
    be distinct and clean

--- ML-Frameworks/pytorch-aarch64/build-wheel.sh
+++ ML-Frameworks/pytorch-aarch64/build-wheel.sh
@@ -20,6 +20,13 @@
 # Note that this script intentionally has no options. It builds *the* PyTorch
 # Tool-Solutions, of which there is only one type
 
+# The logic in this script should mirror the upstream build pipelines as closely
+# as possible, along with Tool-Solutions specific changes we want to test (e.g.
+# installing tbb) or improving local development in a way that doesn't affect
+# the result (e,.g. unsetting MAX_JOBS). Currently the upstream logic is defined
+# in pytorch/.github/workflows/_binary-build-linux.yml and
+# pytorch/.github/workflows/generated-linux-aarch64-binary-manywheel-nightly.yml
+
 set -eux -o pipefail
 
 PYTHON_VERSION="3.10"
@@ -32,8 +39,6 @@ TORCH_BUILD_CONTAINER_ID_FILE="${PWD}/.torch_build_container_id"
 # Output dir for PyTorch wheel and other artifacts
 OUTPUT_DIR=${OUTPUT_DIR:-"${PWD}/results"}
 PYTORCH_FINAL_PACKAGE_DIR=$OUTPUT_DIR
-
-TEST_VENV=aarch64_env_test
 
 PYTORCH_HOST_DIR="${PWD}/pytorch"
 ACL_HOST_DIR="${PWD}/ComputeLibrary"
@@ -50,6 +55,8 @@ fi
 
 if ! docker container inspect $TORCH_BUILD_CONTAINER >/dev/null 2>&1 ; then
 
+    # Based on environment used in pytorch/.github/workflows/_binary-build-linux.yml
+    # and pytorch/.github/workflows/generated-linux-aarch64-binary-manywheel-nightly.yml
     TORCH_BUILD_CONTAINER=$(docker run -t -d \
         -e BINARY_ENV_FILE=/tmp/env \
         -e BUILD_ENVIRONMENT=linux-aarch64-binary-manywheel \
@@ -64,19 +71,22 @@ if ! docker container inspect $TORCH_BUILD_CONTAINER >/dev/null 2>&1 ; then
         -e PYTORCH_EXTRA_INSTALL_REQUIREMENTS="nvidia-cuda-nvrtc-cu12==12.1.105; platform_system == 'Linux' and platform_machine == 'x86_64' | nvidia-cuda-runtime-cu12==12.1.105; platform_system == 'Linux' and platform_machine == 'x86_64' | nvidia-cuda-cupti-cu12==12.1.105; platform_system == 'Linux' and platform_machine == 'x86_64' | nvidia-cudnn-cu12==8.9.2.26; platform_system == 'Linux' and platform_machine == 'x86_64' | nvidia-cublas-cu12==12.1.3.1; platform_system == 'Linux' and platform_machine == 'x86_64' | nvidia-cufft-cu12==11.0.2.54; platform_system == 'Linux' and platform_machine == 'x86_64' | nvidia-curand-cu12==10.3.2.106; platform_system == 'Linux' and platform_machine == 'x86_64' | nvidia-cusolver-cu12==11.4.5.107; platform_system == 'Linux' and platform_machine == 'x86_64' | nvidia-cusparse-cu12==12.1.0.106; platform_system == 'Linux' and platform_machine == 'x86_64' | nvidia-nccl-cu12==2.19.3; platform_system == 'Linux' and platform_machine == 'x86_64' | nvidia-nvtx-cu12==12.1.105; platform_system == 'Linux' and platform_machine == 'x86_64'" \
         -e OPENSSL_ROOT_DIR=/opt/openssl \
         -e CMAKE_INCLUDE_PATH="/opt/openssl/include" \
-        -e TEST_VENV=$TEST_VENV \
         -v "${PYTORCH_HOST_DIR}:${PYTORCH_ROOT}" \
         -v "${PYTORCH_FINAL_PACKAGE_DIR}:/artifacts" \
         -v "${ACL_HOST_DIR}:/ComputeLibrary" \
         -w / \
         "${IMAGE_NAME}")
 
+    # Currently changes in these scripts will not be applied without a clean
+    # build, which is not ideal for dev work. But we have to balance this with
+    # extra time/network traffic when rebuilding many times.
+    docker exec -t $TORCH_BUILD_CONTAINER bash -c $PYTORCH_ROOT/.circleci/scripts/binary_populate_env.sh
     docker exec -t $TORCH_BUILD_CONTAINER bash -c "$PYTORCH_ROOT/.ci/aarch64_linux/aarch64_ci_setup.sh"
-    docker exec -t $TORCH_BUILD_CONTAINER bash -c "python${PYTHON_VERSION} -m venv $TEST_VENV"
-    docker exec -t $TORCH_BUILD_CONTAINER bash -c "source $TEST_VENV/bin/activate && pip install -r $PYTORCH_ROOT/.ci/docker/requirements-ci.txt && pip install ninja==1.10.0.post1"
+
     docker exec -t $TORCH_BUILD_CONTAINER bash -c "yum install -y tbb tbb-devel"
 
-    docker exec -t $TORCH_BUILD_CONTAINER bash /pytorch/.ci/docker/common/install_openblas.sh
+    # This must be in this if block because it cannot handle being called twice
+    docker exec -t $TORCH_BUILD_CONTAINER bash -c "bash $PYTORCH_ROOT/.ci/docker/common/install_openblas.sh"
 
     echo "Storing torch build container id in $TORCH_BUILD_CONTAINER_ID_FILE for reuse: $TORCH_BUILD_CONTAINER"
     echo $TORCH_BUILD_CONTAINER > "$TORCH_BUILD_CONTAINER_ID_FILE"
@@ -84,9 +94,10 @@ else
     docker restart $TORCH_BUILD_CONTAINER
 fi
 
-docker exec -t $TORCH_BUILD_CONTAINER bash -c "rm -rf /pytorch/dist"
+# If there are multiple wheels in the dist directory, an old wheel can be
+# erroneously copied to results, so we clear the directory to be sure
+docker exec -t $TORCH_BUILD_CONTAINER bash -c "rm -rf $PYTORCH_ROOT/dist"
 
-docker exec -t $TORCH_BUILD_CONTAINER bash -c "bash /pytorch/.circleci/scripts/binary_populate_env.sh"
 # We unset OVERRIDE_PACKAGE_VERSION which is written to /tmp/env by
 # populate_binary_env.sh. unsetting makes aarch64_wheel_ci_build.py set it from
 # the last PyTorch commit date. This allows us to fix a torchvision package in
@@ -94,7 +105,8 @@ docker exec -t $TORCH_BUILD_CONTAINER bash -c "bash /pytorch/.circleci/scripts/b
 # We unset MAX_JOBS from 12 (written to /tmp/env by populate_binary_env.sh) to
 # let any downstream functions to decide. Currently nproc when ninja is used,
 # but ideally we would let ninja make the right choice, but nproc is good enough
-docker exec -t $TORCH_BUILD_CONTAINER bash -c "source /tmp/env && unset OVERRIDE_PACKAGE_VERSION MAX_JOBS && bash $PYTORCH_ROOT/.ci/aarch64_linux/aarch64_ci_build.sh"
+docker exec -t $TORCH_BUILD_CONTAINER \
+    bash -c "source /tmp/env && unset OVERRIDE_PACKAGE_VERSION MAX_JOBS && bash $PYTORCH_ROOT/.ci/aarch64_linux/aarch64_ci_build.sh"
 
 # directories generated by the docker container are owned by root, so transfer ownership to user
 docker exec $TORCH_BUILD_CONTAINER chown -R $(id -u):$(id -g) $PYTORCH_ROOT
