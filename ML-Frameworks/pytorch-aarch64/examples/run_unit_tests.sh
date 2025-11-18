bfd6e27032c906e8b100178618c6baf646fed281
Puneet Matharu <puneet.matharu@arm.com>
2025-11-18 13:45:46 +0000

    Remove PyTorch float16 testing with bf16 fpmath mode.

--- ML-Frameworks/pytorch-aarch64/examples/run_unit_tests.sh
+++ ML-Frameworks/pytorch-aarch64/examples/run_unit_tests.sh
@@ -18,13 +18,15 @@
 # *******************************************************************************
 
 if [[ "${ONEDNN_DEFAULT_FPMATH_MODE:-}" == "BF16" ]]; then
-    OMP_NUM_THREADS=16 python -m unittest pytorch/test/test_mkldnn.py -k lower_precision -k bf16 -k bfloat16 -k float16
+    # Run bfloat16 tests but carefully ignore float16
+    OMP_NUM_THREADS=16 python3 -m pytest -q pytorch/test/test_mkldnn.py -k 'bfloat16 or ((lower_precision or bf16) and not float16)'
 else
-    OMP_NUM_THREADS=16 python -m unittest pytorch/test/test_mkldnn.py
+    OMP_NUM_THREADS=16 python3 -m pytest -q pytorch/test/test_mkldnn.py
 fi
 
 if [[ "${ONEDNN_DEFAULT_FPMATH_MODE:-}" == "BF16" ]]; then
-    OMP_NUM_THREADS=16 python -m unittest pytorch/test/test_transformers.py -k bfloat16 -k float16
+    # Run bfloat16 tests but carefully ignore float16
+    OMP_NUM_THREADS=16 python3 -m pytest -q pytorch/test/test_transformers.py -k 'bfloat16 or ((lower_precision or bf16) and not float16)'
 else
-    OMP_NUM_THREADS=16 python -m unittest pytorch/test/test_transformers.py
+    OMP_NUM_THREADS=16 python3 -m pytest -q pytorch/test/test_transformers.py
 fi
