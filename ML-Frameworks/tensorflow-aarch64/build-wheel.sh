e524e0984370a62d89db38519b4dcbc180cad1a3
Puneet Matharu <puneet.matharu@arm.com>
2025-12-11 11:24:44 +0000

    Make project REUSE compliant.

--- ML-Frameworks/tensorflow-aarch64/build-wheel.sh
+++ ML-Frameworks/tensorflow-aarch64/build-wheel.sh
@@ -1,21 +1,8 @@
 #!/bin/bash
 
-# *******************************************************************************
-# Copyright 2024-2026 Arm Limited and affiliates.
+# SPDX-FileCopyrightText: Copyright 2022, 2024-2026 Arm Limited and affiliates.
+#
 # SPDX-License-Identifier: Apache-2.0
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-# *******************************************************************************
 
 # Note that this script intentionally has no options. It builds *the* TensorFlow
 # Tool-Solutions, of which there is only one type
